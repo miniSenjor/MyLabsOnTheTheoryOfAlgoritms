0abc73fda06f3d80a464b83dbf0f8594e825236c
miniSenior <skvorczov2005sergei@gmail.com>
2024-02-01 08:45:34 +0300

    Добавьте файлы проекта.

---
+++ lr3/alPrim/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace alPrim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChangeNumberVertex = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumberVertex = new System.Windows.Forms.TextBox();
+            this.dataGridPrim = new System.Windows.Forms.DataGridView();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.chBxDirection = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridPrim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnChangeNumberVertex
+            // 
+            this.btnChangeNumberVertex.Location = new System.Drawing.Point(65, 79);
+            this.btnChangeNumberVertex.Name = "btnChangeNumberVertex";
+            this.btnChangeNumberVertex.Size = new System.Drawing.Size(108, 23);
+            this.btnChangeNumberVertex.TabIndex = 0;
+            this.btnChangeNumberVertex.Text = "Изменить";
+            this.btnChangeNumberVertex.UseVisualStyleBackColor = true;
+            this.btnChangeNumberVertex.Click += new System.EventHandler(this.btnChangeNumberVertex_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Кол-во вершин:";
+            // 
+            // txtNumberVertex
+            // 
+            this.txtNumberVertex.Location = new System.Drawing.Point(179, 59);
+            this.txtNumberVertex.Name = "txtNumberVertex";
+            this.txtNumberVertex.Size = new System.Drawing.Size(48, 22);
+            this.txtNumberVertex.TabIndex = 2;
+            // 
+            // dataGridPrim
+            // 
+            this.dataGridPrim.AllowUserToAddRows = false;
+            this.dataGridPrim.AllowUserToDeleteRows = false;
+            this.dataGridPrim.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridPrim.Location = new System.Drawing.Point(257, 59);
+            this.dataGridPrim.Name = "dataGridPrim";
+            this.dataGridPrim.RowHeadersWidth = 51;
+            this.dataGridPrim.RowTemplate.Height = 24;
+            this.dataGridPrim.Size = new System.Drawing.Size(378, 271);
+            this.dataGridPrim.TabIndex = 3;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(65, 137);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(108, 23);
+            this.btnCalc.TabIndex = 4;
+            this.btnCalc.Text = "Вычислить";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // chBxDirection
+            // 
+            this.chBxDirection.AutoSize = true;
+            this.chBxDirection.Location = new System.Drawing.Point(65, 109);
+            this.chBxDirection.Name = "chBxDirection";
+            this.chBxDirection.Size = new System.Drawing.Size(150, 21);
+            this.chBxDirection.TabIndex = 5;
+            this.chBxDirection.Text = "есть направление";
+            this.chBxDirection.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chBxDirection);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.dataGridPrim);
+            this.Controls.Add(this.txtNumberVertex);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnChangeNumberVertex);
+            this.Name = "Form1";
+            this.Text = "lr3AlPrim";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridPrim)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChangeNumberVertex;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumberVertex;
+        private System.Windows.Forms.DataGridView dataGridPrim;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.CheckBox chBxDirection;
+    }
+}
+
---
+++ lr3/alPrim/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace alPrim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int numVertex;
+
+        private void btnChangeNumberVertex_Click(object sender, EventArgs e)
+        {
+            dataGridPrim.Rows.Clear();
+            dataGridPrim.Columns.Clear();
+            numVertex = int.Parse(txtNumberVertex.Text);
+            for (int i=0; i<numVertex+1;i++)
+            {
+                dataGridPrim.Columns.Add(new DataGridViewTextBoxColumn()
+                {
+                    HeaderText = i == 0 ? ("") : (Convert.ToChar(64 + i).ToString()),
+                    Name = "Column" + Convert.ToString(i),
+                    ValueType = typeof(int)
+                });
+                if (i>0) dataGridPrim.Rows.Add(Convert.ToChar(64 + i).ToString());
+            }
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            int[,] graph = new int[numVertex,numVertex];
+            for (int i=0;i<graph.GetLength(0);i++)
+                for (int j=0;j<graph.GetLength(0); j++)
+                {
+                    DataGridViewCell cell = dataGridPrim.Rows[i + 1].Cells[j + 1];
+
+                    graph[i, j] = int.Parse(cell.Value.ToString());
+                        //int.Parse(dataGridPrim.Rows[i + 1].Cells[j + 1].Value.ToString());
+                }
+
+            for (int i = 1; i < graph.GetLength(0); i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    Console.WriteLine(i+" "+j);
+                }
+            }
+        }
+    }
+}
