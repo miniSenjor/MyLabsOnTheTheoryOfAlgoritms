6b4ab94ed6d81ee0ada93dce2f38152053f37412
miniSenior <skvorczov2005sergei@gmail.com>
2024-02-01 09:14:18 +0300

    исправил ошибку "не ссылается на экземпляр класса"

--- lr3/alPrim/Form1.cs
+++ lr3/alPrim/Form1.cs
@@ -27,18 +27,21 @@ namespace alPrim
                 });
                 if (i>0) dataGridPrim.Rows.Add(Convert.ToChar(64 + i).ToString());
             }
+            for (int i=0;i<numVertex-1;i++)
+                for (int j=0;j<numVertex-1;j++)
+                {
+                    dataGridPrim.Rows[i + 1].Cells[j + 1].Value = "";
+                }
         }
 
         private void btnCalc_Click(object sender, EventArgs e)
         {
             int[,] graph = new int[numVertex,numVertex];
-            for (int i=0;i<graph.GetLength(0);i++)
-                for (int j=0;j<graph.GetLength(0); j++)
+            for (int i=1; i<graph.GetLength(0); i++)
+                for (int j=1; j<graph.GetLength(0); j++)
                 {
-                    DataGridViewCell cell = dataGridPrim.Rows[i + 1].Cells[j + 1];
-
-                    graph[i, j] = int.Parse(cell.Value.ToString());
-                        //int.Parse(dataGridPrim.Rows[i + 1].Cells[j + 1].Value.ToString());
+                    string cell = dataGridPrim.Rows[i].Cells[j].Value.ToString();
+                    if (cell!="") graph[i, j] = int.Parse(cell);
                 }
 
             for (int i = 1; i < graph.GetLength(0); i++)
